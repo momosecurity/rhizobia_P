832522741726f6ca1a70ec1ed7b98acc4fc5e47a
thecastle <iicoming@hotmail.com>
2019-05-21 16:23:05 +0800

    2019-0521 上传文件安全校验优化、readme更新

--- src/FileSecurity/UploadedFileVerification.php
+++ src/FileSecurity/UploadedFileVerification.php
@@ -14,6 +14,7 @@ namespace Security\FileSecurity;
 class UploadedFileVerification{
 
     /**
+     * 上传文件安全校验
      * @param $file
      * @param $config
      *$config=array('limit'=>5 * 1024 * 1024,
@@ -32,28 +33,32 @@ class UploadedFileVerification{
         if (isset($file["error"]) && $file["error"] > 0) {
             return $data;
         }
-
         if($this->validateFileName($file['name'])!==true){
             $data['info']='文件名包含非法字符！';
             return $data;
         }
-
         $extension=substr(strrchr($file['name'], '.'), 1);
         if(!in_array($extension,array_keys($config['type']))){
             $data['info']='不允许的文件后缀！';
             return $data;
         }
+
+        if(!in_array($file['type'],array_values($config['type']))){
+            $data['info']='不允许的文件格式！';
+            return $data;
+        }
+        if($file['type']!==$config['type'][$extension]){
+            $data['info']='文件后缀与MIME类型不一致！';
+            return $data;
+        }
+
         $size = $file["size"];
         if ($size >= $config['limit']) {
             $data['info']='图片大小不符合要求！';
             return $data;
         }
-        if(!in_array($file['type'],array_values($config['type']))){
-            $data['info']='不允许的文件格式！';
-            return $data;
-        }
+
         $fileAttr = $this->getUploadedFileType($file['tmp_name']);
-        echo  $fileAttr;
         if(!$fileAttr){
             $data['info']='不允许的文件格式！';
             return $data;
@@ -69,6 +74,11 @@ class UploadedFileVerification{
         }
         return $data;
     }
+
+    /** 获取上传文件类型
+     * @param $file 上传文件临时路径
+     * @return mixed
+     */
     private function getUploadedFileType($file){
         $finfo=finfo_open(FILEINFO_MIME_TYPE);
         $mime = finfo_file($finfo,$file);
@@ -76,7 +86,15 @@ class UploadedFileVerification{
         return $mime;
     }
 
-    private function validateFileName( $filename ) {
+    /**
+     * 文件名归一化后，1、如果包含特殊字符，拒绝。2、文件后缀名超过1个，拒绝
+     * @param $file 上传文件名
+     * @return bool
+     */
+    function validateFileName($file ) {
+
+        $parts=explode(".",$file);
+        $filename=array_shift($parts);
         $filename2HtmlEntities = preg_replace("/\\\\u([0-9abcdef]{4})/", "&#x$1;", $filename);
         $filenameUTF8 = mb_convert_encoding($filename2HtmlEntities, 'UTF-8', 'HTML-ENTITIES');
         $specialChars = array( '?', '[', ']', '/', '\\', '=', '<', '>', ':', ';', ',', "'", '"', '&', '$', '#', '*', '(', ')', '|', '~', '`', '!', '{', '}', '%', '+', chr( 0 ) );
@@ -84,6 +102,9 @@ class UploadedFileVerification{
             if(strpos($specialChar,$filenameUTF8)!==false){
                 return false;
             }
+        }
+        if(count($parts)>1){
+            return false;
         }
         return true;
     }
