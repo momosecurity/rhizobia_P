cb321f4375b39592b79bd1a4c571c445ef31095e
thecastle <iicoming@hotmail.com>
2019-05-21 15:28:07 +0800

    2019-0521 上传文件安全校验优化

--- src/FileSecurity/UploadedFileVerification.php
+++ src/FileSecurity/UploadedFileVerification.php
@@ -32,6 +32,12 @@ class UploadedFileVerification{
         if (isset($file["error"]) && $file["error"] > 0) {
             return $data;
         }
+
+        if($this->validateFileName($file['name'])!==true){
+            $data['info']='文件名包含非法字符！';
+            return $data;
+        }
+
         $extension=substr(strrchr($file['name'], '.'), 1);
         if(!in_array($extension,array_keys($config['type']))){
             $data['info']='不允许的文件后缀！';
@@ -46,15 +52,15 @@ class UploadedFileVerification{
             $data['info']='不允许的文件格式！';
             return $data;
         }
-        $fileAttrs = getimagesize($file["tmp_name"]);
-        if(!$fileAttrs){
+        $fileAttr = $this->getUploadedFileType($file['tmp_name']);
+        echo  $fileAttr;
+        if(!$fileAttr){
             $data['info']='不允许的文件格式！';
             return $data;
         }
-        $type = $fileAttrs['mime'];
 
         foreach($config['type'] as $key=> $value){
-            if($type===$value){
+            if($fileAttr===$value){
                 $data['ext']='.'.$key;
                 $data['info']='上传成功！';
                 $data['flag']=true;
@@ -62,5 +68,23 @@ class UploadedFileVerification{
             }
         }
         return $data;
+    }
+    private function getUploadedFileType($file){
+        $finfo=finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_file($finfo,$file);
+        finfo_close($finfo);
+        return $mime;
+    }
+
+    private function validateFileName( $filename ) {
+        $filename2HtmlEntities = preg_replace("/\\\\u([0-9abcdef]{4})/", "&#x$1;", $filename);
+        $filenameUTF8 = mb_convert_encoding($filename2HtmlEntities, 'UTF-8', 'HTML-ENTITIES');
+        $specialChars = array( '?', '[', ']', '/', '\\', '=', '<', '>', ':', ';', ',', "'", '"', '&', '$', '#', '*', '(', ')', '|', '~', '`', '!', '{', '}', '%', '+', chr( 0 ) );
+        foreach ($specialChars as $specialChar){
+            if(strpos($specialChar,$filenameUTF8)!==false){
+                return false;
+            }
+        }
+        return true;
     }
 }
